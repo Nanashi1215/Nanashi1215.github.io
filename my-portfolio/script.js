6b8dfde58a41858b3fb72d3212d2af22ba6f2e15
Nanashi1215 <mcj9363@gmail.com>
2026-08-28 15:40:18 +0000

    any

--- my-portfolio/script.js
+++ my-portfolio/script.js
@@ -26,7 +26,7 @@ const projects = {
         title: "TAMMY QUIZ RUSH: SDG QUEST",
         desc: "A 2D quiz game focused on Sustainable Development Goals.",
         tools: "2D · UNITY",
-        image: "/SDG.gif",
+        image: "./SDG.gif",
         demo: "#",
         github: "https://github.com/JhayD02/Tammy-s-Quiz-Rush-SDG-Quest"
     }
